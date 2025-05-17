669e45614e45828a7f3f7b46055732cb80f4b6fc
dutta-alankar <dutta.alankar@gmail.com>
2025-05-17 09:18:49 +0200

    [FIX] `e_density` correction

--- PLUTOCODE/Src/Cooling/Grackle/grackle_calc.c
+++ PLUTOCODE/Src/Cooling/Grackle/grackle_calc.c
@@ -175,7 +175,8 @@ void call_grackle (const Data *d, double dt, timeStep *Dts, Grid *grid)
             grackle_chemistry_fields.HeI_density[id] = (1.0 - grackle_config_data->HydrogenFractionByMass) * grackle_chemistry_fields.density[id];
             grackle_chemistry_fields.HeII_density[id] = tiny_number * grackle_chemistry_fields.density[id];
             grackle_chemistry_fields.HeIII_density[id] = tiny_number * grackle_chemistry_fields.density[id];
-            grackle_chemistry_fields.e_density[id] = (grackle_chemistry_fields.HII_density[id] + (grackle_chemistry_fields.HeII_density[id] + 2*grackle_chemistry_fields.HeIII_density[id])/4)*(CONST_me/CONST_mp); 
+            grackle_chemistry_fields.e_density[id] = (grackle_chemistry_fields.HII_density[id] + (grackle_chemistry_fields.HeII_density[id] + 2*grackle_chemistry_fields.HeIII_density[id])/4); // *(CONST_me/CONST_mp); 
+	    // normalization: see https://grackle.readthedocs.io/en/latest/Interaction.html#density-note
         }
         if (grackle_config_data->primordial_chemistry >= 2) {
             grackle_chemistry_fields.HM_density[id] = tiny_number * grackle_chemistry_fields.density[id];
