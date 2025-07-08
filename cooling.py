fa88f0ba1c98efa2bfed1ecd7c26d1c8b6755d00
dutta-alankar <dutta.alankar@gmail.com>
2025-07-08 17:12:47 +0200

    [ENH] multi metallicity cooling plot

--- cooling.py
+++ cooling.py
@@ -8,15 +8,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from plot_style import *
 
-data = np.loadtxt("./outputs/analysis.dat")
+metals = [0.3, 0.5, 1.0, 3.0]
 
-plt.plot(data[1:,1], data[1:,3], color=colors[1])
+for i, metal in enumerate(metals):
+    data = np.loadtxt(f"./outputs/analysis-Z{metal:.1f}.dat")
+    plt.plot(data[1:,1], data[1:,3], color=colors[i], label=f"{metal:.1f}")
 
 plt.xlabel(r"Temperature [K]")
 plt.ylabel(r"$\Lambda = -\dot{e}/n_H^2$ [$\rm erg\ cm^3 \ s^{-1}$]")
+plt.legend(loc="best", fancybox=True, title=r"$Z/Z_\odot$", framealpha=0.6, ncol=2)
 
-# plt.xlim(xmin=-0.1, xmax=25)
-# plt.ylim(ymin=2.0e-02, ymax=14.0) 
+plt.xlim(xmin=1.0e+04, xmax=2.1e+06)
+plt.ylim(ymin=5e-24, ymax=3e-21) 
 
 plt.xscale("log")
 plt.yscale("log")
